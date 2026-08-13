bf81f39f948bb0ec8bb18a5d0690b7e419d7db01
Anushka Wijesundara <anushka@wijesundara.com>
2026-08-14 00:39:48 +0900

    Initial Commit

---
+++ ServiceOps/ServiceOpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceOpsApp.swift
+//  ServiceOps
+//
+//  Created by Anushka Wijesundara on 2026/08/14.
+//
+
+import SwiftUI
+
+@main
+struct ServiceOpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
